3887a0b3749edb5bf23032d213aee4bb4aa60dfa
Helena Nunes <helenanc@uol.com.br>
2016-11-16 23:41:20 -0300

    HTTP - erro

--- LojaApp/LojaApp/MainPage.xaml.cs
+++ LojaApp/LojaApp/MainPage.xaml.cs
@@ -1,11 +1,15 @@
 ﻿using LojaApp.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -29,7 +33,7 @@ namespace LojaApp
             PopularCB();
         }
 
-        //private string ip = "http://192.168.0.23";
+        private string ip = "http://192.168.0.23";
 
         private void PopularCB()
         {
@@ -150,21 +154,125 @@ namespace LojaApp
             listVeicVend.ItemsSource = db.Veiculos.Where(x => x.Vendido == true).ToList();
         }
 
+        /* SERVIÇO HTTP */
+
+        /* DISPOSITIVO PARA SERVIDOR */
+
+        private async void UparFab()
+        {
+            //faz contato com o servidor
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(ip);
+
+            //entra no servidor e pega uma lista de fabricantes
+            var response = await httpClient.GetAsync("/api/fabricante/");
+            var str = response.Content.ReadAsStringAsync().Result;
+            List<Fabricante> obj = JsonConvert.DeserializeObject<List<Fabricante>>(str);
+
+            //deleta todos os fabricantes do servidor de acordo com a lista obtida
+            foreach (Fabricante f in obj)
+                await httpClient.DeleteAsync("/api/fabricante/" + f.Id.ToString());
+
+            //pega a lista do dispositivo local (loja db), serializa e insere no servidor
+            Loja db = new Loja();
+            string s = "=" + JsonConvert.SerializeObject(db.Fabricantes.ToList());
+            var content = new StringContent(s, Encoding.UTF8, "application/x-www-form-urlencoded");
+            await httpClient.PostAsync("/api/fabricante/", content);
+        }
+        private async void UparVei()
+        {
+            //faz contato com o servidor
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(ip);
+
+            //entra no servidor e pega uma lista de veiculos
+            var response = await httpClient.GetAsync("/api/veiculo/");
+            var str = response.Content.ReadAsStringAsync().Result;
+            List<Veiculo> obj = JsonConvert.DeserializeObject<List<Veiculo>>(str);
+
+            //deleta todos os veiculos do servidor de acordo com a lista obtida
+            foreach (Veiculo v in obj)
+                await httpClient.DeleteAsync("/api/veiculo/" + v.Id.ToString());
+
+            //pega a lista do dispositivo local (loja db), serializa e insere no servidor
+            Loja db = new Loja();
+            string s = "=" + JsonConvert.SerializeObject(db.Veiculos.ToList());
+            var content = new StringContent(s, Encoding.UTF8, "application/x-www-form-urlencoded");
+            await httpClient.PostAsync("/api/veiculo/", content);
+        }
+
+        /* SERVIDOR PARA DISPOSITIVO */
+
+        private async void AtualizarFab()
+        {
+            // Acessa os dados do serviço para recuperar a lista
+            // de fabricantes
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(ip);
+            var response = await httpClient.GetAsync("/api/fabricante/");
+            var str = response.Content.ReadAsStringAsync().Result;
+
+            // Converte o json do serviço para uma lista
+            List<Fabricante> obj = JsonConvert.DeserializeObject<List<Fabricante>>(str);
+
+            // Apaga os dados locais na tabela de fabricantes
+            Loja db = new Loja();
+            db.Fabricantes.RemoveRange(db.Fabricantes.ToArray());
+            db.SaveChanges();
+
+            // Grava a lista de fabricates que veio do serviço
+            // no banco local
+            db.Fabricantes.AddRange(obj);
+            db.SaveChanges();
+
+            // Mostra a lista no listview
+            listFab.ItemsSource = null;
+            listFab.ItemsSource = db.Fabricantes.ToList();
+        }
+        private async void AtualizarVei()
+        {
+            // Acessa os dados do serviço para recuperar a lista
+            // de veiculos
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(ip);
+            var response = await httpClient.GetAsync("/api/veiculo/");
+            var str = response.Content.ReadAsStringAsync().Result;
+
+            // Converte o json do serviço para uma lista
+            List<Veiculo> obj = JsonConvert.DeserializeObject<List<Veiculo>>(str);
+
+            // Apaga os dados locais na tabela de fabricantes
+            Loja db = new Loja();
+            db.Veiculos.RemoveRange(db.Veiculos.ToArray());
+            db.SaveChanges();
+
+            // Grava a lista de fabricates que veio do serviço
+            // no banco local
+            db.Veiculos.AddRange(obj);
+            db.SaveChanges();
+
+            // Mostra a lista no listview
+            listVeic.ItemsSource = null;
+            listVeic.ItemsSource = db.Veiculos.ToList();
+        }
+
+        /* BOTÕES - ATUALIZAR SERVIÇO */
+
         private async void btnADispositivo_Click(object sender, RoutedEventArgs e)
-        {/*
+        {
             AtualizarFab();
             AtualizarVei();
             PopularCB();
             MessageDialog dlg = new MessageDialog("Dispositivo atualizado com sucesso!");
-            await dlg.ShowAsync();*/
+            await dlg.ShowAsync();
         }
 
         private async void btnAServidor_Click(object sender, RoutedEventArgs e)
-        {/*
+        {
             UparFab();
             UparVei();
             MessageDialog dlg = new MessageDialog("Servidor atualizado com sucesso!");
-            await dlg.ShowAsync();*/
+            await dlg.ShowAsync();
         }
     }
 }
